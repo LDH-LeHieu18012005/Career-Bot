4d8c78a7304fb39a45e944ed3569ef9e6ec24f19
LDH-LeHieu18012005 <hieuleduonghy@gmail.com>
2026-04-03 08:19:12 +0700

    🧭 Khởi tạo Hệ thống đánh giá và lập lộ trình phát triển (Advisor)

---
+++ career_guidance/__init__.py
@@ -0,0 +1,3 @@
+from .skill_extractor import SkillExtractor, SKILL_DICT, extract_skills_from_text
+from .market_analyzer import MarketAnalyzer, detect_field
+from .advisor import CareerAdvisor, SYSTEM_GUIDANCE, _parse_skills_from_input, _detect_experience_level
